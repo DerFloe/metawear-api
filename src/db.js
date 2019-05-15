b789a13b3f7b6f8c5a0085417b4dd16627ee9822
Florian Hofer <derfloe11@gmail.com>
2019-05-15 19:02:33 +0200

    Implemented preliminary database model implemented queries for all tables implemented add mutations

--- src/db.js
+++ src/db.js
@@ -38,9 +38,74 @@ const Season = Conn.define('season', {
         type: Sequelize.STRING
     }
 });
+
 Season.hasMany(Item);
 Item.belongsTo(Season);
 
-Conn.sync({force: false});
+const Brand = Conn.define('brand', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING
+    }
+});
+
+Brand.hasMany(Item);
+Item.belongsTo(Brand);
+
+const Store = Conn.define('store', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    link: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    logo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const Storelisting = Conn.define('storelisting', {
+    price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    saleprice: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    link: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const Stock = Conn.define('stock', {
+    size: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    stockcount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+Storelisting.belongsTo(Item);
+Item.hasMany(Storelisting);
+
+
+Stock.belongsTo(Storelisting)
+Storelisting.hasMany(Stock)
+
+Storelisting.belongsTo(Store)
+Store.hasMany(Storelisting)
+
+Conn.sync({force: true});
 
 export default Conn;
--- src/schema.js
+++ src/schema.js
@@ -4,6 +4,7 @@ import {
     GraphQLSchema,
     GraphQLInt,
     GraphQLString,
+    GraphQLFloat,
     GraphQLList
 } from 'graphql';
 import Db from './db';
@@ -48,6 +49,12 @@ const Item = new GraphQLObjectType({
                 resolve(item) {
                     return item.getSeason();
                 }
+            },
+            brand: {
+                type: Brand,
+                resolve(item) {
+                    return item.getBrand();
+                }
             }
             
         }
@@ -76,6 +83,9 @@ const Mutation = new GraphQLObjectType({
                     },
                     season: {
                         type: GraphQLInt
+                    },
+                    brand: {
+                        type: GraphQLInt
                     }
                 },
                 resolve(_, args){
@@ -84,7 +94,113 @@ const Mutation = new GraphQLObjectType({
                         description: args.description,
                         color: args.color,
                         image: args.image,
-                        seasonId: args.season
+                        seasonId: args.season,
+                        brandId: args.brand
+                    })
+                }
+            },
+            addSeason: {
+                type: Season,
+                args: {
+                    title: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    description: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    }
+                },
+                resolve(_, args){
+                    return Db.models.season.create({
+                        title: args.title,
+                        description: args.description
+                    })
+                }
+            },
+            addBrand: {
+                type: Brand,
+                args: {
+                    name: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    description: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    }
+                },
+                resolve(_, args){
+                    return Db.models.brand.create({
+                        name: args.name,
+                        description: args.description
+                    })
+                }
+            },
+            addStore: {
+                type: Store,
+                args: {
+                    name: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    link: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    logo: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    }
+                },
+                resolve(_, args){
+                    return Db.models.store.create({
+                        name: args.name,
+                        link: args.link,
+                        logo: args.logo
+                    })
+                }
+            },
+            addStock: {
+                type: Stock,
+                args: {
+                    size:{
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    stockcount: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    storelisting: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    }
+                },
+                resolve(_, args){
+                    return Db.models.stock.create({
+                        size: args.size,
+                        stockcount: args.stockcount,
+                        storelistingId: args.storelisting
+                    })
+                }
+            },
+            addStorelisting: {
+                type: Storelisting,
+                args: {
+                    price: {
+                        type: new GraphQLNonNull(GraphQLFloat)
+                    },
+                    saleprice: {
+                        type: new GraphQLNonNull(GraphQLFloat)
+                    },
+                    link: {
+                        type: new GraphQLNonNull(GraphQLString)
+                    },
+                    item: {
+                        type: new GraphQLNonNull(GraphQLInt)
+                    },
+                    store:{
+                        type: new GraphQLNonNull(GraphQLInt)
+                    }
+                },
+                resolve(_, args){
+                    return Db.models.storelisting.create({
+                        price: args.price,
+                        saleprice: args.saleprice,
+                        link: args.link,
+                        itemId: args.item,
+                        storeId: args.store
                     })
                 }
             }
@@ -99,26 +215,181 @@ const Season = new GraphQLObjectType({
         return {
             id: {
                 type: GraphQLInt,
-                resolve(description){
-                    return description.id;
+                resolve(season){
+                    return season.id;
                 }
             },
             title: {
                 type: GraphQLString,
-                resolve(description){
-                    return description.title;
+                resolve(season){
+                    return season.title;
                 }
             },
             description: {
                 type: GraphQLString,
-                resolve(description){
-                    return description.description;
+                resolve(season){
+                    return season.description;
                 }
             },
             items: {
                 type: new GraphQLList(Item),
-                resolve(item) {
-                    return item.getSeasons();
+                resolve(season) {
+                    return season.getItems();
+                }
+            }
+            
+        }
+    }
+});
+
+const Stock = new GraphQLObjectType({
+    name: 'Stock',
+    description: 'A stock entry',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(stock){
+                    return stock.id;
+                }
+            },
+            size: {
+                type: GraphQLString,
+                resolve(stock){
+                    return stock.size;
+                }
+            },
+            stockcount: {
+                type: GraphQLInt,
+                resolve(stock){
+                    return stock.stockcount;
+                }
+            },
+            storelistings: {
+                type: Storelisting,
+                resolve(stock){
+                    return stock.getStorelisting();
+                }
+            }
+        }
+    }
+});
+
+
+
+const Brand = new GraphQLObjectType({
+    name: 'Brand',
+    description: 'A brand',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(brand){
+                    return brand.id;
+                }
+            },
+            name: {
+                type: GraphQLString,
+                resolve(brand){
+                    return brand.name;
+                }
+            },
+            description: {
+                type: GraphQLString,
+                resolve(brand){
+                    return brand.description;
+                }
+            },
+            items: {
+                type: new GraphQLList(Item),
+                resolve(brand) {
+                    return brand.getItems();
+                }
+            }
+            
+        }
+    }
+});
+
+const Storelisting = new GraphQLObjectType({
+    name: 'Storelisting',
+    description: 'A storelisting',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(storelisting){
+                    return storelisting.id;
+                }
+            },
+            price: {
+                type: GraphQLFloat,
+                resolve(storelisting){
+                    return storelisting.price;
+                }
+            },
+            saleprice: {
+                type: GraphQLFloat,
+                resolve(storelisting){
+                    return storelisting.saleprice;
+                }
+            },
+            link: {
+                type: GraphQLString,
+                resolve(storelisting) {
+                    return store.link;
+                }
+            },
+            stocks: {
+                type: new GraphQLList(Stock),
+                resolve(storelisting){
+                    return storelisting.getStocks();
+                }
+            },
+            item: {
+                type: Item,
+                resolve(storelisting){
+                    return storelisting.getItem();
+                }
+            },
+            store: {
+                type: Store,
+                resolve(storelisting){
+                    return storelisting.getStore();
+                }
+            }
+            
+        }
+    }
+});
+
+const Store = new GraphQLObjectType({
+    name: 'Store',
+    description: 'A store',
+    fields: () => {
+        return {
+            id: {
+                type: GraphQLInt,
+                resolve(store){
+                    return store.id;
+                }
+            },
+            name: {
+                type: GraphQLString,
+                resolve(store){
+                    return store.name;
+                }
+            },
+            link: {
+                type: GraphQLString,
+                resolve(store){
+                    return store.link;
+                }
+            },
+            logo: {
+                type: GraphQLString,
+                resolve(store) {
+                    return store.logo;
                 }
             }
             
@@ -154,7 +425,31 @@ const Query = new GraphQLObjectType({
             seasons: {
                 type: new GraphQLList(Season),
                 resolve(root,args){
-                    return Db.models.seasons.findAll({where: args});
+                    return Db.models.season.findAll({where: args});
+                }
+            },
+            brands: {
+                type: new GraphQLList(Brand),
+                resolve(root,args){
+                    return Db.models.brand.findAll({where: args});
+                }
+            },
+            stores: {
+                type: new GraphQLList(Store),
+                resolve(root,args){
+                    return Db.models.store.findAll({where: args});
+                }
+            },
+            stocks: {
+                type: new GraphQLList(Stock),
+                resolve(root,args){
+                    return Db.models.stock.findAll({where: args});
+                }
+            },
+            storelistings: {
+                type: new GraphQLList(Storelisting),
+                resolve(root,args){
+                    return Db.models.storelisting.findAll({where: args});
                 }
             }
         }
